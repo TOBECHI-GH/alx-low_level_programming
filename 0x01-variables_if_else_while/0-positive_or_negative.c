b0415c1bb05223b203a618144d0c729672a6381c
Tobechukwu Onwuazombe <tobechi300@gmail.com>
2022-03-13 03:18:19 +0100

    Assign random number to n on each execution

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,22 +3,22 @@
 #include <stdlib.h>
 /**
  * main - main block
- * Description: %i is format specifier that detects the base automatically
+ * Description: %i is format specifier that detects the base automatically.
+ * No need for condition in else statement
  * Return: 0
  */
 int main(void)
 {
-	
         int n;
 
         srand(time(0));
         n = rand() - RAND_MAX / 2;
         if (n > 0) 
-            printf("%i is positive\n", n);
+                printf("%i is positive\n", n);
         else if (n < 0) 
-            printf("%i is negative\n", n);
-        else (n == 0) 
-            printf("%i is zero\n", n);
+                printf("%i is negative\n", n);
+        else
+                printf("%i is zero\n", n);
         return (0);
 }
 

c7567d6078c06b5e029874c72d6016dbab2432e0
Tobechukwu Onwuazombe <tobechi300@gmail.com>
2022-03-13 02:58:15 +0100

    Assign random number to n on each execution

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,26 +8,17 @@
 int main(void)
 {
 	
-	int n;
+        int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
 
-	if (n > 0) 
-    {
-	printf("%d is positive\n", n);
-	}
-
-	else if (n == 0) 
-    {
-	printf("%d is zero\n", n);
-	}
-
-	else (n < 0) 
-    {
-	printf("%d is negative\n", n);
-	}
-
-	return (0);
-
+        if (n > 0) 
+            printf("%i is positive\n", n);
+        else if (n == 0) 
+            printf("%i is zero\n", n);
+        else (n < 0) 
+            printf("%i is negative\n", n);
+        return (0);
 }
+

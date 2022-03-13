fd76618a75602b8b676ac05e97b1fcb1780fca29
Tobechukwu Onwuazombe <tobechi300@gmail.com>
2022-03-13 22:19:34 +0100

    Print base 16 numbers in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print all numbers base 16 in lowercase (hexadecimal)
+ * How can characters accept numbers as value
+ * Return: 0
+ */
+int main(void)
+{
+char i;
+for (i = '0'; i <= '15'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}

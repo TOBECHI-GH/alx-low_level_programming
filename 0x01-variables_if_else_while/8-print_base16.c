b34ace91a19fca332a3fc1787158cc936044e5d7
Tobechukwu Onwuazombe <tobechi300@gmail.com>
2022-03-13 22:33:45 +0100

    Print base 16 numbers in lowercase

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,11 +7,17 @@
  */
 int main(void)
 {
-int i;
-for (i = '0'; i <= '15'; i++)
+int c;
+
+for (c = 48; c <= 57; c++)
 {
-putchar(i);
+putchar(c);
 }
-putchar('\n');
+
+for (c = 97; c <= 102; c++)
+{
+putchar(c);
+}
+putchar(10);
 return (0);
 }

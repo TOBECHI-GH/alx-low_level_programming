d8865970213c73b3eb78fa2fc03022fa0e1c4cd5
Tobechukwu Onwuazombe <tobechi300@gmail.com>
2022-03-13 17:50:04 +0100

    Print lowercase of character except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<ctype.h>
+/**
+ * main - main block
+ * Description: Lowercase of character except q and e
+ * Return: 0
+ */
+int main(void)
+{
+int ch = 'A'
+while ((isalpha(ch) != 'Q') && (isalpha(ch) != 'E'))
+{
+putchar(tolower(ch));
+ch++; 
+}
+putchar('\n');
+return (0);
+}

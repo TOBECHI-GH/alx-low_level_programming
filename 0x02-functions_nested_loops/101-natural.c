d49dfd06eabdbf05f6225daab78f9f1b70d8e426
Tobechukwu Onwuazombe <tobechi300@gmail.com>
2022-03-17 22:15:08 +0100

    Print sum of multiple of 3 and 5 below 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - main block
+* Description: computes and prints the sum of all the multiples of 3 or
+* 5 below 1024 (excluded), followed by a new line
+* Return: 0
+*/
+int main(void)
+{
+int i;
+int sum = 0;
+
+for (i = 1; i < 1024; i++) 
+{
+if (i % 3 == 0 || i % 5 == 0)
+{
+sum = sum + i
+}
+
+}
+printf("%i\n", sum);
+return (0);
+}
